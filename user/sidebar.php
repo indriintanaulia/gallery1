02825753089048adb94765d196d02480938ecd19
indriintanaulia <caesarradit02@gmail.com>
2024-04-19 00:37:31 -0700

    first commit

---
+++ user/sidebar.php
@@ -0,0 +1,38 @@
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <a style="color:white;" class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <small> Album Gallery</small>
+    </a>
+
+    <hr class="sidebar-divider my-0">
+
+    <li class="nav-item active">
+        <a style="color:cyan;" class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-home"></i>
+            <span>Menu Utama</span></a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+
+    <li class="nav-item active">
+        <a style="color:cyan;" class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+            <i class="fas fa-users"></i>
+            <span>Data Gallery</span>
+        </a>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="album.php">Album</a>
+                <a class="collapse-item" href="foto.php">Foto</a>
+            </div>
+        </div>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+
+</ul>
